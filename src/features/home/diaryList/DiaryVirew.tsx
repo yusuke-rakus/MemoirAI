f13d4e542269e1570b0e6b7e35fc073fc32e01a1
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2025-06-09 22:57:17 +0900

    Improve: 日記一覧

--- src/features/home/diaryList/DiaryVirew.tsx
+++ src/features/home/diaryList/DiaryVirew.tsx
@@ -1,4 +1,5 @@
 import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { useDiaryList } from "../hooks/useDiaryList";
 import { DiaryItem } from "./components/DiaryItem";
 
@@ -9,12 +10,13 @@ export const DiaryVirew = () => {
   // console.log(loading);
 
   return (
-    <>
-      <Card className="w-full items-start p-4 md:p-6 gap-4 md:gap-6">
-        {dialies.map((diary) => {
-          return <DiaryItem diary={diary} />;
-        })}
-      </Card>
-    </>
+    <Card className="w-full items-start p-4 md:p-3 gap-4 md:gap-6">
+      {dialies.map((diary, index) => (
+        <div key={diary.id}>
+          <DiaryItem diary={diary} />
+          {index < dialies.length - 1 && <Separator className="my-4" />}
+        </div>
+      ))}
+    </Card>
   );
 };
