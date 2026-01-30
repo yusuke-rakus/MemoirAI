04db23cac39e3496fd4a841980544447ad6003da
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2026-01-31 00:40:00 +0900

    feat: 日記リスト画面にEmptyを表示

--- src/features/diaries/components/DiariesView.tsx
+++ src/features/diaries/components/DiariesView.tsx
@@ -1,6 +1,7 @@
 import { useFetchDiary } from "../hooks/useFetchDiary";
 import { useDiaryDetailStore } from "../provider/DiaryDetailProvider";
 import { DiaryPreviewCard } from "./DiaryPreviewCard";
+import { EmptyDiaries } from "./EmptyDiaries";
 
 export const DiariesView = () => {
   const { uploadedDiaries } = useDiaryDetailStore();
@@ -8,7 +9,7 @@ export const DiariesView = () => {
 
   return (
     <div className="max-w-4xl mx-auto">
-      {uploadedDiaries.length > 0 && (
+      {uploadedDiaries.length > 0 ? (
         <div>
           <div className="space-y-4">
             {uploadedDiaries.map((diary, i) => (
@@ -16,6 +17,8 @@ export const DiariesView = () => {
             ))}
           </div>
         </div>
+      ) : (
+        <EmptyDiaries />
       )}
     </div>
   );
